84967cae313fcca2f8a1a94804272e060f3b2d8d
Mehir Kumar <mehirk28@gmail.com>
2025-02-12 20:48:16 -0400

    added sum functions

--- week-4-todo-app/index.js
+++ week-4-todo-app/index.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) =>{
   res.send('Hello World')
 })
+
+function sum(a, b) {
+  return a+b;
+}
+
+const sum = (a, b) => {
+  a = a + 1
+  return a + b
+}
+
 
 app.listen(3000)
