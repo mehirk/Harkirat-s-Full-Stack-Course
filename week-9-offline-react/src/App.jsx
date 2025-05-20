a0594291cca2b3e916e4d573587d7e15287f0c4e
Mehir Kumar <mehirk28@gmail.com>
2025-05-20 13:58:56 +0400

    formatted the document properly

--- week-9-offline-react/src/App.jsx
+++ week-9-offline-react/src/App.jsx
@@ -1,75 +1,81 @@
 function App() {
-	return (
-		<div style={
-			{
-				background: "#dfe6e9",
-				height: "100vh"
-			}
-		}>
-			<div style={
-				{
-					display: "flex",
-					justifyContent: "center"
-				}
-			}>
-				<PostComponent name={"Mehir"}
-					followerCount={"53,456"}
-					time={"15m"}
-					image={"https://media.licdn.com/dms/image/v2/D4E03AQHOlXfaN1DDRw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1726586797040?e=1753315200&v=beta&t=Cfthp8erdQMh6fgABUqioYKWThI6cxZpOGRy7KW4Lo0"}
-					description={"Want to know how to win big? Check out these folks won $6000 in bounties."}/>
-			</div>
-		</div>
-	)
+    // return JSX that will be rendered
+    return (
+        // Apply inline styles to the div element
+        <div style={{ backgroundColor: "#dfe6e9", height: "100vh" }}>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <div>
+                    <div>
+                        {/* Call PostComponent here with props to render it in the App component */}
+                        <PostComponent
+                            name={"Bharat"}
+                            followerCount={20}
+                            time={"2m ago"}
+                            image={"https://imgs.search.brave.com/F5OoHgfmJlOgbB00VWzyvvcxuQZk7JOPrCYV51JMmNU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTI3/Mzc5NDUyL3Bob3Rv/L2tpdHR5LmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz13UWcw/TXdTcXJxZkkzWHdN/WGREVjhkY2RZMlNH/WVh2YndPdE12cUxX/UWpJPQ"}
+                            description={"What to know how to win big? Check out how these folks won $6000 in bounties."}
+                        />
+                    </div>
+                    <div>
+                        <div>
+                            {/* Call PostComponent here with props to render it in the App component */}
+                            <PostComponent
+                                name={"Harkirat"}
+                                followerCount={30}
+                                time={"3m ago"}
+                                image={"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"}
+                                description={"How to get hired in 2024? I lost my Job in 2023, this is the roadmap I followed to get hired in 2024."}
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            {/* Call PostComponent here with props to render it in the App component */}
+                            <PostComponent
+                                name={"Deepak"}
+                                followerCount={50}
+                                time={"18m ago"}
+                                image={"https://imgs.search.brave.com/ZACv93qZO57A2RrexnAjJi9CTpejuyu2aIGeB9-2beA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJhY2Nlc3Mu/Y29tL2Z1bGwvMTk0/OTM2My5qcGc"}
+                                description={"Here is the roadmap to become a full-stack developer in 2024."}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
+// Create a style object to apply styles to the div element in PostComponent
 const style = {
-	width: 200,
-	backgroundColor: "white",
-	borderRadius: 10,
-	borderColor: "gray",
-	borderWidth: 1,
-	padding: 20,
-	margin: 10
+    width: 250,
+    backgroundColor: "white",
+    borderRadius: 10,
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 20,
+    margin: 10,
+};
+
+// Create a function component named PostComponent with props to render it in the App component
+function PostComponent({ name, followerCount, time, image, description }) {
+    // return JSX that will be rendered
+    return (
+        // Apply style object to the div element
+        <div style={style}>
+            {/* display the name, followerCount, time, image, and description using props */}
+            <div style={{ display: "flex" }}>
+                <img src={image} style={{ width: 40, height: 40, borderRadius: 40 }} />
+                <div style={{ fontSize: 14, marginLeft: 10 }}>
+                    <b>{name}</b>
+                    <div>{followerCount} followers</div>
+                    <div>{time}</div>
+                </div>
+            </div>
+
+            <div style={{ fontSize: 14 }}>{description}</div>
+        </div>
+    );
 }
 
-function PostComponent({
-	name,
-	followerCount,
-	time,
-	image,
-	description
-}) {
-	return (
-		<div style={style}>
-			<div style={
-				{display: "flex"}
-			}>
-				<img src={image}
-					style={
-						{
-							width: 30,
-							height: 30,
-							borderRadius: 20
-						}
-					}/>
-				<div style={
-					{
-						fontSize: 10,
-						marginLeft: 10
-					}
-				}>
-					<b> {name} </b>
-					<div>{followers}</div>
-					<div>{time}</div>
-				</div>
-			</div>
-			<div style={
-				{fontStyle: 12}
-			}>
-				{description} </div>
-		</div>
-	);
-}
-
-
-export default App
+// Export App Component to use it in other components
+export default App;
