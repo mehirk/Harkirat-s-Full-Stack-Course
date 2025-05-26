6f80bc676c2d68cd2efb1f00b14f95e7b41546b5
Mehir Kumar <mehirk28@gmail.com>
2025-05-26 14:21:09 +0400

    understood useEffect in a better way, implemented rendering on changing tab with a loading skeleton

--- week-9-offline-react/src/App.jsx
+++ week-9-offline-react/src/App.jsx
@@ -1,39 +1,72 @@
-import { PostComponent } from "./Posts";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
+    const [currentTab, setCurrentTab] = useState(1);
+    const [tabData, setTabData] = useState({});
+    const [loading, setLoading] = useState(true);
 
-    const [posts, setPosts] = useState([])
+    useEffect(() => {
+        setLoading(true);
+        fetch(`https://jsonplaceholder.typicode.com/posts/${currentTab}`)
+            .then(async res => {
+                const json = await res.json();
+                setTabData(json);
+                setLoading(false);
+            })
+    }, [currentTab]);
 
-    const postComponents = posts.map(post => <PostComponent
-        name={post.name}
-        subtitle={post.subtitle}
-        time={post.time}
-        image={post.image}
-        description={post.description}
-    />)
-
-    function addPost() {
-        setPosts([...posts, {
-            name: "mehir",
-            subtitle: "10,000 followers",
-            time: "2m ago",
-            image: "https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg",
-            description: "Want to know how to win big? Check out how these folks won $6000 in bounties."
-
-        }])
-    }
-
-    return (
-        <div style={{ background: "#dfe6e9", height: "100vh", }}>
-            <button onClick={addPost}>Add Post</button>
-            <div style={{ display: "flex", justifyContent: "center" }}>
-                <div>
-                    {postComponents}
-                </div>
-            </div>
-        </div>
-    )
+    return <div>
+        <button onClick={() => setCurrentTab(1)} style={{ color: currentTab == 1 ? "red" : "black" }}>Todo #1</button>
+        <button onClick={() => setCurrentTab(2)} style={{ color: currentTab == 2 ? "red" : "black" }}>Todo #2</button>
+        <button onClick={() => setCurrentTab(3)} style={{ color: currentTab == 3 ? "red" : "black" }}>Todo #3</button>
+        <button onClick={() => setCurrentTab(4)} style={{ color: currentTab == 4 ? "red" : "black" }}>Todo #4</button>
+        <br />
+        {loading ? "Loading..." : tabData.title}
+    </div>
 }
 
 export default App
+
+
+
+
+
+
+// import { PostComponent } from "./Posts";
+
+// function App() {
+
+//     const [posts, setPosts] = useState([])
+
+//     const postComponents = posts.map(post => <PostComponent
+//         name={post.name}
+//         subtitle={post.subtitle}
+//         time={post.time}
+//         image={post.image}
+//         description={post.description}
+//     />)
+
+//     function addPost() {
+//         setPosts([...posts, {
+//             name: "mehir",
+//             subtitle: "10,000 followers",
+//             time: "2m ago",
+//             image: "https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg",
+//             description: "Want to know how to win big? Check out how these folks won $6000 in bounties."
+
+//         }])
+//     }
+
+//     return (
+//         <div style={{ background: "#dfe6e9", height: "100vh", }}>
+//             <button onClick={addPost}>Add Post</button>
+//             <div style={{ display: "flex", justifyContent: "center" }}>
+//                 <div>
+//                     {postComponents}
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default App
