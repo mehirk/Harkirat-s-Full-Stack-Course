8e6dacf18aa75d766b2cd138c2ec60c71a7a4dfa
Mehir Kumar <mehirk28@gmail.com>
2025-06-24 19:39:49 +0400

    changes

--- week-13-tailwind/src/App.jsx
+++ week-13-tailwind/src/App.jsx
@@ -1,37 +1,26 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='text-center'>
+        Webinar.gg
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
 
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className='text-center text-3xl font-semibold m-10'>
+        Verify Your Age
+      </div>
+
+      <div className='text-center text-xs font-light'>
+        Please confirm your birth year. This data will not be stored.
+      </div>
+
+      <div className='text-center '>
+        <input className='rounded-md border-3 border-gray-700 focus:border-pink-600 text-sm font-semibold font-extralight m-2 p-2' type="number" placeholder='Your birth year'></input>
+      </div>
     </>
   )
 }
